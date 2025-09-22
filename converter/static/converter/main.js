2458f326f6f1d84fff235e71e974a0e89c96b4df
1No1Name1 <mateuszkielan38@gmail.com>
2025-09-22 13:14:05 +0200

    Update main.js

--- converter/static/converter/main.js
+++ converter/static/converter/main.js
@@ -45,7 +45,7 @@ function triggerNQuadsConversion() {
     })
     .then(response => response.json())
     .then(data => {
-        alert(data.message);  // Replace with styled popup if needed
+        alert(data.message);  
     })
     .catch(error => {
         console.error('Conversion error:', error);
