6039bec0346e9a9a4be4e16d1d2b1da4c337dfcf
MateuszKielan <mateuszkielan38@gmail.com>
2025-09-30 21:45:39 +0200

    Trying to get the vocabulary card work.

--- converter/urls.py
+++ converter/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('convert/', views.convert_screen_view, name='convert_screen'),
     path('convert/nquads/', convert_to_nquads_view, name='convert_nquads'),
     path('replace_all/', views.replace_all, name='replace_all'),
-    path('request_type/<str:request_type>', views.set_request_type, name='set_request_type')
+    path('request_type/<str:request_type>', views.set_request_type, name='set_request_type'),
 ]
